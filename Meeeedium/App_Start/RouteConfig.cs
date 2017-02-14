e76d06406bc719a1fcab05048897c7a96edc651e
Michael Anderson <Micmail566@gmail.com>
2017-02-13 22:03:19 -0500

    Set up url shortener and added url and username link with  /u and /r

--- Meeeedium/App_Start/RouteConfig.cs
+++ Meeeedium/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace Meeeedium
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
